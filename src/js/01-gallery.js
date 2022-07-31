21b08dde8689ef5c2450094d28d3633b0111d0e8
Olena Nesteruk <pavlena81@gmail.com>
2022-07-31 18:26:10 +0300

    add gallery and form modal

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+ 
+const gallaryEl = document.querySelector('.gallery');
+function createListGalleryItems(items) {
+    // event.preventDefault();
+    return items.map(({preview, original, description}) => `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join('');
+}
 
+const listGallaryItems = createListGalleryItems(galleryItems);
+gallaryEl.insertAdjacentHTML("beforeend", listGallaryItems);
+
+gallaryEl.addEventListener('click', selectImgGallery);
+
+function selectImgGallery(event) {
+    if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+}
 console.log(galleryItems);
+
+
+// ================basiclightbox======================
+const modalTemplate = () =>  
+    `<div class='modal'>
+        <p>I'm a modal created from a DOM element/node.</p>
+    </div>`;
+
+const instance = basicLightbox.create(modalTemplate());
+
+// instance.show();
+// Создание и рендер разметки по массиву данных galleryItems и 
+// предоставленному шаблону элемента галереи.
+// Реализация делегирования на div.gallery и получение url большого
+// изображения.
+// Подключение скрипта и стилей библиотеки модального окна basicLightbox.
+//  Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные(.min) файлы библиотеки.
+// Открытие модального окна по клику на элементе галереи. 
+// Для этого ознакомься с документацией и примерами.
+// Замена значения атрибута src элемента < img > в модальном окне перед
+// открытием.Используй готовую разметку модального окна с изображением
+//  из примеров библиотеки basicLightbox.
+
