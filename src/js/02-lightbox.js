81aea75aa653a6cc5a53af6e6a5accb29fe41d63
Olena Nesteruk <pavlena81@gmail.com>
2022-08-29 17:04:21 +0300

    Update 02-lightbox.js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -15,13 +15,13 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const ulEl = document.querySelector('.gallery');
-function createListGalleryItems(items) {
+  function createListGalleryItems(items) {
     return items.map(({ preview, original, description }) => `
     <a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>`).join('');
 }
-const listGallaryItems = createListGalleryItems(galleryItems);
+   const listGallaryItems = createListGalleryItems(galleryItems);
 ulEl.insertAdjacentHTML("beforeend", listGallaryItems);
 // ulEl.addEventListener('click', onSelectImgGallery);
 console.log(galleryItems);
